b3316cac393a227a1975249ff048826bd2b1cc85
pranav dodiya <pranavdodiya72@gmail.com>
2022-06-17 11:59:44 +0530

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    before_action :abc, if: :devise_controller?
+
+    protected
+    def abc
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :contact_number)}
+    end
+
+    
 end
