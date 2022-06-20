207d0ec5513cb986de315cd39d3eb47e2fad6c86
Vaisvik-Chaudhary <vaisvik.chaudhary@bacancy.com>
2022-06-20 16:44:57 +0530

    Implemented reviews and rating functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   has_and_belongs_to_many :carts
   has_many :cartitems
   has_many_attached :foodimages
+  has_many :reviews, :as => :reviewable
   
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_19_163909) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_080424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,6 +99,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_19_163909) do
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.float "rating"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "reviewable_type", null: false
+    t.bigint "reviewable_id", null: false
+    t.bigint "restaurant_id"
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+    t.index ["reviewable_type", "reviewable_id"], name: "index_reviews_on_reviewable"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "userdetails", force: :cascade do |t|
     t.string "username"
     t.string "user_address"
@@ -140,5 +156,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_19_163909) do
   add_foreign_key "carts", "users"
   add_foreign_key "items", "restaurants"
   add_foreign_key "restaurants", "users"
+  add_foreign_key "reviews", "items"
+  add_foreign_key "reviews", "restaurants"
+  add_foreign_key "reviews", "users"
   add_foreign_key "userdetails", "users"
 end
