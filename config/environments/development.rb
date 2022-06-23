59ec42002c5aa4128c37b9cb9bf97020fcc6e4ed
Vaisvik-Chaudhary <vaisvik.chaudhary@bacancy.com>
2022-06-23 23:57:54 +0530

    Deletion of migration files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_options = {cc:'vaisvik.chaudhary@bacancy.com','pranav.dodiya@bacancy.com']}
+  config.action_mailer.default_options = {cc:['vaisvik.chaudhary@bacancy.com','pranav.dodiya@bacancy.com']}
 
   config.action_mailer.smtp_settings = {
     
@@ -125,18 +125,18 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host:   ENV['MAIL_HOST']}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:          ENV['MAIL_HOST'],
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host:   ENV['MAIL_HOST']}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      ENV['SENDMAIL_USERNAME'],
+  #   password:       ENV['SENDMAIL_PASSWORD'],
+  #   domain:          ENV['MAIL_HOST'],
+  #   address:       'smtp.gmail.com',
+  #   port:          '587',
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
 end
--- db/migrate/20220620080424_add_reviewable_to_reviews.rb
+++
@@ -1,8 +0,0 @@
-class AddReviewableToReviews < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :reviews, :reviewable, polymorphic: true, null: false
-    add_reference :reviews, :restaurant, foreign_key: true
-    add_reference :reviews, :item, foreign_key: true
-    add_reference :reviews, :user, foreign_key: true
-  end
-end
